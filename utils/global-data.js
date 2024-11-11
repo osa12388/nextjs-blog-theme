145f82eb0f22988e861e8f661f8eac2d8365bc2a
osa12388 <37342144+osa12388@users.noreply.github.com>
2024-11-11 20:01:50 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Kamperem po Europie | Blog';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Wszystkie prawa zastrzeżone 2024-2025 | Więcej o nas';
 
   return {
     name,
