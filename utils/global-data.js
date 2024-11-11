f21dd1aaf6c095c9e05c8b2579e2f5772d917b2c
osa12388 <37342144+osa12388@users.noreply.github.com>
2024-11-11 20:44:37 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Kamperem po Europie | Blog';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Blog o podróżowaniu po Europie. Kamperem i nie tylko';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
      : 'Wszystkie prawa zastrzeżone 2024-2025 | Więcej o nas<br><a href="https://example.com" target="_blank" rel="noopener noreferrer">Odwiedź naszą stronę</a>';
